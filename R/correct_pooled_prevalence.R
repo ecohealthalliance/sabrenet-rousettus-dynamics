22b6c73a23abb60df8449580253f0c4150488494
Noam Ross <ross@ecohealthalliance.org>
2022-10-20 09:42:49 -0400

    Add pooled prevalence correction

---
+++ R/correct_pooled_prevalence.R
@@ -0,0 +1,14 @@
+# Pooled prevalence correction for pools of equal size
+# From Burrows PM (1987). Improved estimation of pathogen transmission rates by
+# group testing. Phytopathology 77 363–365.
+correct_pooled_prevalence <- function(num_pos_pools, num_pools, pool_size) {
+  x <- num_pos_pools
+  n <- num_pools
+  m <- pool_size
+
+  a <- 0.5*(m-1)/m
+
+  p_hat <- 1 - ((n - x + a)/(n + a))^(1/m)
+
+  p_hat
+}
